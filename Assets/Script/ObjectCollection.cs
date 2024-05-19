03934b0f9e12a116a01f5c1b2c28452c40fcfbab
KyAMogollon <adhair.mogollon@tecsup.edu.pe>
2024-05-18 19:30:46 -0500

    UIGame

---
+++ Assets/Script/ObjectCollection.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectCollection : MonoBehaviour
+{
+    public int piecesToCollected = 3;
+    private int piecesCollected = 0;
+    [SerializeField] GameManager _gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gm.UpdatePiecesText(piecesCollected, piecesToCollected);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Piezas"))
+        {
+            
+            piecesCollected++;
+            _gm.UpdatePiecesText(piecesCollected, piecesToCollected);
+            Destroy(collision.gameObject);
+            if(piecesCollected >= piecesToCollected)
+            {
+                Debug.Log("Se abrio la puerta");
+            }
+        }
+    }
+}
